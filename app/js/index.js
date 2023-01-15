9bb4608080082618c451a60badbe2160dbf6efb5
Hosseinreza <hrakbari77@gmail.com>
2023-01-15 14:34:56 +0330

    Fix the first image with random number

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,19 @@
-var randomNumber1 = Math.floor(Math.random() * 6 ) + 1;
-console.log(randomNumber1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+if ( randomNumber1 == 1){
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png")
+}
+if ( randomNumber1 == 2){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice2.png")
+}
+if ( randomNumber1 == 3){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice3.png")
+}
+if ( randomNumber1 == 4){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice4.png")
+}
+if ( randomNumber1 == 5){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice5.png")
+}
+if ( randomNumber1 == 6){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice6.png")
+}
