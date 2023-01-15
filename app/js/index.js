64624a36cf68155ed36d0d27f32479c2b5260f40
Hosseinreza <hrakbari77@gmail.com>
2023-01-15 14:38:30 +0330

    Change to clear code

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-if ( randomNumber1 == 1){
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png")
-}
-if ( randomNumber1 == 2){
-    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice2.png")
-}
-if ( randomNumber1 == 3){
-    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice3.png")
-}
-if ( randomNumber1 == 4){
-    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice4.png")
-}
-if ( randomNumber1 == 5){
-    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice5.png")
-}
-if ( randomNumber1 == 6){
-    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice6.png")
-}
+var images = ["dice1","dice2","dice3","dice4","dice5","dice6"];
+var image = images[randomNumber1-1];
+var imageValue = "images/" + image + ".png";
+document.querySelector(".img1").setAttribute("src", imageValue);
+
