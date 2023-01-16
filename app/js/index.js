adb5504030291d0944400b2a41244f4d5bfed89b
Hosseinreza <hrakbari77@gmail.com>
2023-01-16 14:51:23 +0330

    Add message for winner

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,23 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var images = ["dice1","dice2","dice3","dice4","dice5","dice6"];
-var image = images[randomNumber1-1];
-var imageValue = "images/" + image + ".png";
+const images = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"];
+const imageIndex1 = images[randomNumber1 - 1];
+var imageValue = "images/" + imageIndex1 + ".png";
 document.querySelector(".img1").setAttribute("src", imageValue);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-image = images[randomNumber2-1];
-imageValue = "images/" + image + ".png";
+const imageIndex2 = images[randomNumber2 - 1];
+imageValue = "images/" + imageIndex2 + ".png";
 document.querySelector(".img2").setAttribute("src", imageValue);
+
+const messages = ["🚩Player1 Wins!", "Player2 Wins!🚩", "Draw!"];
+var msg = "";
+if ( imageIndex1 > imageIndex2 ){
+    msg = messages[0];
+}
+else if ( imageIndex1 < imageIndex2 ) {
+    msg = messages[1]
+}
+else {
+    msg = messages[2];
+}
+document.querySelector("h1").textContent = msg;
